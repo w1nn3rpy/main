2449939199f4ae2fe55d8270dcd61f58e9a990de
w1nn3rpy <l33sa1337@gmail.com>
2024-06-15 13:26:39 +0400

    redef dict.get()

---
+++ Mod 12/My Dict/main.py
@@ -0,0 +1,31 @@
+class MyDict(dict):
+    """
+    Класс словаря для переопределения метода get
+    """
+    def get(self, value):
+        if value in self.keys():
+            return self[value]
+        else:
+            return 0
+
+
+"""Переопределенный словарь"""
+
+mydict = MyDict({'a': 2, 'b': 3, 'c': 5})
+print(mydict)
+for i_dict in mydict.items():
+    print(i_dict)
+print(mydict.get('a'))
+print(mydict.get('c'))
+print(mydict.get('test'))  # Возвращает 0
+
+
+"""Обычный словарь"""
+
+original_dict = dict({'a': 2, 'b': 3, 'c': 5})
+print(original_dict)
+for i_dict in original_dict.items():
+    print(i_dict)
+print(original_dict.get('a'))
+print(original_dict.get('c'))
+print(original_dict.get('test'))  # Возвращает None

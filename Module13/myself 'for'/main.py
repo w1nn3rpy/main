e20da6982b5b8b9b1e94baec918f27b7b9a27282
Dmitry <165276328+w1nn3rpy@users.noreply.github.com>
2024-06-16 12:07:11 +0400

    Delete Module13/myself 'for' directory

--- Module13/myself 'for'/main.py
+++
@@ -1,9 +0,0 @@
-test = [x for x in range(10)]
-print(test)
-iterator = iter(test)
-while True:
-    try:
-        print(iterator.__next__())
-    except StopIteration:
-        print('Итератор списка пуст.')
-        break
